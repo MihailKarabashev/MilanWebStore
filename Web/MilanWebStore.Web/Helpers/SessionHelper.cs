21902e4f9e9046a8dcfb5df80bf8a61215d9bc61
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-05 13:21:17 +0300

    Create session helper

---
+++ Web/MilanWebStore.Web/Helpers/SessionHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace MilanWebStore.Web.Helpers
+{
+    using Microsoft.AspNetCore.Http;
+    using Newtonsoft.Json;
+
+    public static class SessionHelper
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value, new JsonSerializerSettings { }));
+        }
+
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
