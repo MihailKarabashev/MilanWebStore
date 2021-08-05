9d6803a1414b807aeee1bf4a6f680d5625c35495
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-05 13:14:54 +0300

    Add hangfire authorization filter

---
+++ Web/MilanWebStore.Web/Filters/HangfireAuthorizationFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace MilanWebStore.Web.Filters
+{
+    using Hangfire.Dashboard;
+
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            return httpContext.User.IsInRole("Administrator");
+        }
+    }
+}
