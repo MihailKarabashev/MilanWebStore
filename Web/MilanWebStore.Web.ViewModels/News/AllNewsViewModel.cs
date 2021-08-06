f4e31f39e349c07ddfacf1549940417c42af9f52
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-06 10:34:30 +0300

    Add news controller

---
+++ Web/MilanWebStore.Web.ViewModels/News/AllNewsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MilanWebStore.Web.ViewModels.News
+{
+    using System.Collections.Generic;
+
+    public class AllNewsViewModel : PagingViewModel
+    {
+        public IEnumerable<SingleNewsViewModel> AllNews { get; set; }
+    }
+}
---
+++ Web/MilanWebStore.Web/Controllers/NewsController.cs
@@ -0,0 +1,39 @@
+﻿namespace MilanWebStore.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MilanWebStore.Common;
+    using MilanWebStore.Services.Data.Contracts;
+    using MilanWebStore.Web.ViewModels.News;
+
+    public class NewsController : BaseController
+    {
+        private readonly INewsService newsService;
+
+        public NewsController(INewsService newsService)
+        {
+            this.newsService = newsService;
+        }
+
+        public IActionResult All(int id = 1)
+        {
+            var model = new AllNewsViewModel()
+            {
+                AllNews = this.newsService.GetAll<SingleNewsViewModel>(id, GlobalConstants.ItemsPerPage),
+                ItemsPerPage = GlobalConstants.ItemsPerPage,
+                PageNumber = id,
+                ProductsCount = this.newsService.GetNewsCount(),
+            };
+
+            this.ViewData["Name"] = "News";
+
+            return this.View(model);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var news = this.newsService.ById<SingleNewsViewModel>(id);
+
+            return this.View(news);
+        }
+    }
+}
