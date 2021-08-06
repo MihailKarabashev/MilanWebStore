0390f4ec8d83db82b978ace2e2075da8f1149b9b
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-06 09:50:27 +0300

    Implement address controller

---
+++ Web/MilanWebStore.Web/Controllers/AddressesController.cs
@@ -0,0 +1,38 @@
+﻿namespace MilanWebStore.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using MilanWebStore.Data.Models;
+    using MilanWebStore.Services.Data.Contracts;
+    using MilanWebStore.Web.ViewModels.Addresses;
+
+    public class AddressesController : BaseController
+    {
+        private readonly IAddressesService addressesService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AddressesController(
+            IAddressesService addressesService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.addressesService = addressesService;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Add(AddressViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction(nameof(OrdersController.Create));
+            }
+
+            var user = await this.userManager.GetUserAsync(this.User);
+
+            await this.addressesService.CreateAsync(model, user.Id);
+
+            return this.RedirectToAction(nameof(OrdersController.Create));
+        }
+    }
+}
