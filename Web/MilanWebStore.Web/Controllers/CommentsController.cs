bd4d5ebc110f2ab81a78a7a2547b755be8588f0a
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-06 10:22:51 +0300

    Add comment and favorites controllers

---
+++ Web/MilanWebStore.Web/Controllers/CommentsController.cs
@@ -0,0 +1,33 @@
+﻿namespace MilanWebStore.Web.Controllers
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using MilanWebStore.Services.Data.Contracts;
+
+    public class CommentsController : BaseController
+    {
+        private readonly ICommentsService commentsService;
+
+        public CommentsController(ICommentsService commentsService)
+        {
+            this.commentsService = commentsService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(int productId, string commentContent, int categoryId)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            await this.commentsService.CreateAsync(productId, userId, commentContent);
+            return this.RedirectToAction("ById", "Products", new { id = productId, categoryId = categoryId });
+        }
+    }
+}
---
+++ Web/MilanWebStore.Web/Controllers/FavoritesController.cs
@@ -0,0 +1,51 @@
+﻿namespace MilanWebStore.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using MilanWebStore.Data.Models;
+    using MilanWebStore.Services.Data.Contracts;
+    using MilanWebStore.Web.ViewModels.Favorites;
+
+    [Authorize]
+    public class FavoritesController : BaseController
+    {
+        private readonly IFavoriteProductsService favoritesService;
+        private readonly UserManager<ApplicationUser> usermanger;
+
+        public FavoritesController(
+            IFavoriteProductsService favoritesService,
+            UserManager<ApplicationUser> usermanger)
+        {
+            this.favoritesService = favoritesService;
+            this.usermanger = usermanger;
+        }
+
+        public IActionResult Index()
+        {
+            var userFavoriteProducts = new AllFavoriteProductsViewModel()
+            {
+                Favorites = this.favoritesService.All<FavoriteProductViewModel>(this.User.Identity.Name),
+            };
+
+            return this.View(userFavoriteProducts);
+        }
+
+        public async Task<IActionResult> Add(int id)
+        {
+            var user = await this.usermanger.GetUserAsync(this.User);
+            await this.favoritesService.AddAsync(user.Id, id);
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.favoritesService.DeleteAsync(id);
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
