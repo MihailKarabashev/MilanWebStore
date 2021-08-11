2b11291f059d66d3ef918bf16d4b2124f191e02c
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-11 15:31:38 +0300

    Add Votes service tests

---
+++ Tests/MilanWebStore.Services.Data.Tests/VotesServiceTests.cs
@@ -0,0 +1,74 @@
+﻿namespace MilanWebStore.Services.Data.Tests
+{
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.EntityFrameworkCore;
+    using MilanWebStore.Data;
+    using MilanWebStore.Data.Models;
+    using MilanWebStore.Data.Repositories;
+    using MilanWebStore.Services.Mapping;
+    using MilanWebStore.Web.ViewModels;
+    using Xunit;
+
+    public class VotesServiceTests
+    {
+        public VotesServiceTests()
+        {
+            this.InitializeMapper();
+        }
+
+        [Fact]
+        public async Task VoteAsyncShouldAddVoteToDataBaseCorrectly()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            using var dbContext = new ApplicationDbContext(options);
+
+            using var repository = new EfRepository<Vote>(dbContext);
+
+            var service = new VotesService(repository);
+
+            await service.VoteAsync(1, "test", 3);
+            var expected = new Vote
+            {
+                Id = 1,
+                ApplicationUserId = "test",
+                Value = 3,
+            };
+
+            var count = await dbContext.Votes.CountAsync();
+            var actual = await dbContext.Votes.FirstOrDefaultAsync();
+
+            count.Should().Be(1);
+            expected.Value.Should().Be(actual.Value);
+
+        }
+
+        [Fact]
+        public async Task GetAverageVoteShouldWorksCorrectly()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            using var dbContext = new ApplicationDbContext(options);
+
+            using var repository = new EfRepository<Vote>(dbContext);
+
+            var service = new VotesService(repository);
+
+            await service.VoteAsync(1, "test", 3);
+            await service.VoteAsync(1, "anotherTest", 3);
+
+            var averageVote = service.GetAverageVote(1);
+
+            averageVote.Should().Be(3);
+        }
+
+
+        private void InitializeMapper() => AutoMapperConfig.
+          RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+    }
+}
