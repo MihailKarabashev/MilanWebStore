3d773b1ce73c77ade662fff67850bb313db1e5ab
mihail-karabashev <mihailkarabashev@gmail.com>
2021-08-12 13:00:52 +0300

    Update Settings service tests

--- Tests/MilanWebStore.Services.Data.Tests/SettingsServiceTests.cs
+++ Tests/MilanWebStore.Services.Data.Tests/SettingsServiceTests.cs
@@ -22,7 +22,7 @@
         {
             var repository = new Mock<IDeletableEntityRepository<Setting>>();
 
-            repository.Setup(r => r.All()).Returns(new List<Setting>
+            repository.Setup(r => r.AllAsNoTracking()).Returns(new List<Setting>
                                                         {
                                                             new Setting(),
                                                             new Setting(),
@@ -32,35 +32,26 @@
             var service = new SettingsService(repository.Object);
 
             Assert.Equal(3, service.GetCount());
-
-            repository.Verify(x => x.All(), Times.Once);
         }
 
         [Fact]
         public async Task GetCountShouldReturnCorrectNumberUsingDbContext()
         {
-            // Правим опции за създаване
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: "SettingsTestDb").Options;
 
-            // създаваме базата
             using var dbContext = new ApplicationDbContext(options);
 
-            //добавяме към базата
             dbContext.Settings.Add(new Setting());
             dbContext.Settings.Add(new Setting());
             dbContext.Settings.Add(new Setting());
 
-            //сейфаме
             await dbContext.SaveChangesAsync();
 
-            //създаваме репосизитори от тип делитаблеРепозитори от сеттинг
             using var repository = new EfDeletableEntityRepository<Setting>(dbContext);
 
-            // правим инстанция на сървис класа -> сеттингс сървис и му подаваме репозитори
             var service = new SettingsService(repository);
 
-            //проверка
             Assert.Equal(3, service.GetCount());
         }
     }
